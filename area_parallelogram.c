92a8f5f707ff8466e8c57ac8c589ffac8437780e
Jarrod Hatting <jarrodhatting@icloud.com>
2019-11-19 15:02:22 -0800

    update

--- area_parallelogram.c
+++ area_parallelogram.c
@@ -6,7 +6,7 @@
 
 * Creation Date : 06-11-2019
 
-* Last Modified : Wed Nov  6 12:11:25 2019
+* Last Modified : Tue Nov 19 15:01:46 2019
 
 * Created By : Jarrod Hatting
 _._._._._._._._._._._._._._._._._._._._._.*/
@@ -16,13 +16,18 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 
 int main ()
 {
-double base = 12.0;
-double height = 5.0;
-double area = 0.0;
+    double base = 0.0;
+    double height = 0.0;
+    double area = 0.0;
 
-area = base * height;
+    printf("What is the length of the base of the Parallelogram: ");
+    scanf("%lf", &base);
 
-printf("Area is : %.2fsqin\n", area);
+    printf("What is the length of the height of the Parallelogram: ");
+    scanf("%lf", &height);
+
+    area = base * height;
+    printf("Area is : %.2fsqin\n", area);
 
 return (0);
 }
