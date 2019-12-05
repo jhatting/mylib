dd89f47a2b1a4c41da4d92d1424411990c8a6bb4
Jarrod Hatting <jarrodhatting@icloud.com>
2019-12-05 12:12:15 -0800

    update

--- loopPrintNum.c
+++ loopPrintNum.c
@@ -18,26 +18,26 @@ Natural numbers from 1 to 10: 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
 * Created By : Jarrod Hatting
 _._._._._._._._._._._._._._._._._._._._._.*/
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-int main ()
+int main()
 {
-    int i =  1;
+    int i = 1;
     int n = 0;
-// Input of the limit from the user
+
     printf("Upper limit: ");
     scanf("%d", &n);
-    printf("Natural numbe%s from 1 to %d: ", n == 1 ? "r" : "rs", n);
-    
-    while (i<n)
+    printf("Natural numbe%s from 1 to %d: ", n == 1 ? "r": "rs", n);
+
+    while (i < n)
     {
         printf("%d, ", i);
         i++;
     }
     while (i == n)
     {
-        printf("%d\n" , i);
+        printf("%d\n", i);
         i++;
     }
 
