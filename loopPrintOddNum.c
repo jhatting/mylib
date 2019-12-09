c9bec83372fc772812f03dca432ca05b96ce5f7b
Jarrod Hatting <jarrodhatting@icloud.com>
2019-12-09 13:43:19 -0800

    update

---
+++ loopPrintOddNum.c
@@ -0,0 +1,28 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : loopPrintOddNum.c
+
+* Purpose :print all odd numbers from 1 to n using while loop
+Input:upper limit: 10
+Output:Odd numbers between 1 to 10:
+1, 3, 5, 7, 9
+
+* Creation Date : 09-12-2019
+
+* Last Modified :
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n = 0;
+    int i = 0;
+
+    printf("Upper limit: ");
+    scanf("%d", &n);
+return (0);
+}
