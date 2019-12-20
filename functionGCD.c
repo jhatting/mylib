fde4ea8e07e8bb9cae97df47928d810cea69d83b
Jarrod Hatting <jarrodhatting@icloud.com>
2019-12-20 12:53:21 -0800

    update

---
+++ functionGCD.c
@@ -0,0 +1,43 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : functionGCD.c
+
+* Purpose :This is a function that calculates the Greatest Common Divisor
+            GCD, using a function.
+
+* Creation Date : 19-12-2019
+
+* Last Modified : Fri Dec 20 12:51:33 2019
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int gcd(int u, int v);
+
+int main()
+{
+    int a,b,g;
+    printf("Enter GCD  #a and #b:\n");
+    scanf("%d%d", &a, &b);
+
+    g = gcd(a, b);
+    printf("GCD(%d,%d) = %d",a ,b ,g );
+
+    return (0);
+}
+
+int gcd(int u, int v)
+{
+    int temp;
+
+    while(v !=0)
+        {
+            temp = u % v;
+            u = v;
+            v = temp;
+        }
+        return (u);
+}
