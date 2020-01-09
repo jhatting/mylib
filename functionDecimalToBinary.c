e1ff47c624a869f4c702af58a373dd27379395d5
Jarrod Hatting <jarrodhatting@icloud.com>
2020-01-09 10:40:34 -0800

    update

---
+++ functionDecimalToBinary.c
@@ -0,0 +1,25 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : functionDecimalToBinary.c
+
+* Purpose : Write a program in C to convert decimal number to binary number using the function.
+ Test Data :
+ Input any decimal number : 65
+ Expected Output :
+
+ The Binary value is : 1000001 
+
+* Creation Date : 09-01-2020
+
+* Last Modified :
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <stdio.h>
+
+int main()
+{
+    return(0);
+}
+
