f7fcdd2b0f5a3378af1ff1fcd5fad7c3a0dcd9e1
Jarrod Hatting <jarrodhatting@icloud.com>
2020-01-21 15:53:51 -0800

    update

---
+++ characterTest.c
@@ -0,0 +1,20 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : characterTest.c
+
+* Purpose : testing the forward slashes \\
+
+* Creation Date : 21-01-2020
+
+* Last Modified : Tue Jan 21 13:54:25 2020
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("For \" you write\\\".");
+    return(0);
+}
