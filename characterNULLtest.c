79574236dd7818b3705dfef3501b3003ddb39221
Jarrod Hatting <jarrodhatting@icloud.com>
2020-01-21 16:10:47 -0800

    update

---
+++ characterNULLtest.c
@@ -0,0 +1,20 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : characterNULLtest.c
+
+* Purpose : this is testing the effect NULL has on the string output
+
+* Creation Date : 21-01-2020
+
+* Last Modified : Tue Jan 21 16:10:25 2020
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <stdio.h>
+
+int main()
+{
+    printf("The Character \0 is used to terminate a string.");
+    return(0);
+}
