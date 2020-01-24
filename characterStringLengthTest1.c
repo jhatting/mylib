40005ff9175868f405c71417e3d243b64b4125f1
Jarrod Hatting <jarrodhatting@icloud.com>
2020-01-24 14:29:09 -0800

    update

---
+++ characterStringLengthTest1.c
@@ -0,0 +1,23 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : characterStringTest.c
+
+* Purpose : Testing Strings
+
+* Creation Date : 24-01-2020
+
+* Last Modified : Fri Jan 24 14:21:34 2020
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main ()
+{
+    char myString[] = "My name is JARBROG";
+
+    printf("The length is %lu", strlen(myString));
+    return (0);
+}
