4f625df44d16340131996008a973cb41734b3d75
Jarrod Hatting <jarrodhatting@icloud.com>
2020-01-28 19:33:07 -0800

    update

---
+++ characterStringConcatenation.c
@@ -0,0 +1,29 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : characterStringConcatenation.c
+
+* Purpose :akes two strings for arguments
+A copy of the second string is tackled onto the end of the first 
+This combined version becomes the new first string
+The second string is not altered
+
+* Creation Date : 28-01-2020
+
+* Last Modified :
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char myString[] = "My string";
+    char input[80];
+
+    printf("Enter a string to be concatenated: ");
+    scanf("%s", input);
+
+    printf(")
+}
