f9e62b2aa08b588b85f0fb3600ba275c7b49f441
Jarrod Hatting <jarrodhatting@icloud.com>
2020-02-03 14:18:18 -0800

    update

--- characterStringCompare.c
+++ characterStringCompare.c
@@ -20,6 +20,20 @@ _._._._._._._._._._._._._._._._._._._._._.*/
 int main()
 {
     printf("strcmp(\"A\",\"A\") is ");
-    printf("%dn", strcmp("A", "A"));
+    printf("%d\n", strcmp("A", "A"));
 
+    printf("strcmp(\"A\",\"B\") is ");
+    printf("%d\n", strcmp("A", "B"));
+
+    printf("strcmp(\"B\",\"A\") is ");
+    printf("%d\n", strcmp("B", "A"));
+
+    printf("strcmp(\"C\",\"A\") is ");
+    printf("%d\n", strcmp("C", "A"));
+
+    printf("strcmp(\"Z\",\"a\") is ");
+    printf("%d\n", strcmp("Z", "a"));
+
+    printf("strcmp(\"apples\",\"apple\") is ");
+    printf("%d\n", strcmp("apples", "apple"));
 }
