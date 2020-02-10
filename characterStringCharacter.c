bff776fc4a37b1d5dc22f9cb6e10ca4b78d351cc
Jarrod Hatting <jarrodhatting@icloud.com>
2020-02-10 14:58:36 -0800

    update

---
+++ characterStringCharacter.c
@@ -0,0 +1,16 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : characterStringCharacter.c
+
+* Purpose : first argument to strchr() is the address of the first location to be searched.
+- second argument is the character that is sought (ch, which is of type char)
+-expects its second argument to be of type int , so the compiler will convert the value of ch to this type
+-could just as well define ch as type int (int ch ='q';)
+-pGot_char will point to the value ("quick brown fox")
+
+* Creation Date : 10-02-2020
+
+* Last Modified :
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
