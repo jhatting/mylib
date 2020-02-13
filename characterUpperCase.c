4cbf679349524ba41277700b2b3861dba1213f0f
Jarrod Hatting <jarrodhatting@icloud.com>
2020-02-13 11:30:00 -0800

    update

---
+++ characterUpperCase.c
@@ -0,0 +1,28 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : characterUpperCase.c
+
+* Purpose :This loop will convert the entire string in the buf array to uppercase by stepping through the string one character at a time 
+
+* Creation Date : 13-02-2020
+
+* Last Modified : Thu Feb 13 11:29:22 2020
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    int i = 0;
+    buf = "Jarrod";
+    A = 0;
+
+    while (buf[i] = (char) toupper(buf [i] != ‘\0’)
+        ++i;
+        printf("%s", toupper(buf, A);
+
+    return (0);
+}
