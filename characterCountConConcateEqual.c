03cdad5c8be9bfdda44185754f73764081b4f1f4
Jarrod Hatting <jarrodhatting@icloud.com>
2020-02-19 14:26:33 -0800

    update

---
+++ characterCountConConcateEqual.c
@@ -0,0 +1,16 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : characterCountConConcateEqual.c
+
+* Purpose : Function counts the number of characters in a string(length)
+-cannot use the strlen function
+-function should take a character array as a parameter
+-Should return an int (the length)
+
+
+* Creation Date : 19-02-2020
+
+* Last Modified :
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
