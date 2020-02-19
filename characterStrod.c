60078c7a8d5aa608b544be5ce57e628e71a67bcf
Jarrod Hatting <jarrodhatting@icloud.com>
2020-02-18 16:26:09 -0800

    update

---
+++ characterStrod.c
@@ -0,0 +1,13 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : characterStrod.c
+
+* Purpose : A value of type double is produced from the initial part of the string specified by the first
+argument. The second argument is a pointer to a variable, ptr say , of type char* in which the function will store the address of the first character following the substring that was converted to the double value. If no string was found that could be converted to type double , the variable ptr will contain the address  passed as the first argument
+
+* Creation Date : 18-02-2020
+
+* Last Modified : Tue Feb 18 16:26:06 2020
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
