f521da365020270a649a0228e1b3f5307049d616
Jarrod Hatting <jarrodhatting@icloud.com>
2020-02-20 11:37:17 -0800

    update

---
+++ characterStringReverse.c
@@ -0,0 +1,30 @@
+/* -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.
+
+* File Name : characterStringReverse.c
+
+* Purpose : A Program that displays a string in reverse
+    - Should read input from the Keyboard
+    - Need to use the strlen string function.
+
+-Program that sorts the strings of an array using a bubble sort.
+    -Need to use the strcmp and strcpy functions.
+
+    input number of strings: 3 
+    input strings 3:
+    zero
+    one 
+    two
+
+    Expected output:
+
+    the Strings appears after sorting:
+    one 
+    two 
+    zero
+
+* Creation Date : 20-02-2020
+
+* Last Modified :
+
+* Created By : Jarrod Hatting
+_._._._._._._._._._._._._._._._._._._._._.*/
